279fa023bb99d4f3e7f03a5bf625e3ff4f15689c
Fang Yi <yifang@faceunity.com>
2019-02-15 18:05:24 +0800

    增加?判断可选参数，支持判断@MaxLength,@MinLength,@Max,@Min,@Isin装饰器生成参数范围

--- src/ApiClassParser.ts
+++ src/ApiClassParser.ts
@@ -1,106 +1,126 @@
-import { Project } from 'ts-simple-ast'
+import { Project } from "ts-simple-ast";
 
 type IElement = {
-  content: string
-  name: string
-  source: string
-  sourceName: string
-}
+	content: string;
+	name: string;
+	source: string;
+	sourceName: string;
+};
 
 /**
  * Parser for class
  */
 export class ApiClassParser {
-  protected type: string
-  protected filePath: string
-  protected className: string
-  private ast
-  private typeMap = {
-    'apiSuccessClass': 'apiSuccess',
-    'apiParamClass': 'apiParam'
-  }
+	protected type: string;
+	protected filePath: string;
+	protected className: string;
+	private ast;
+	private typeMap = {
+		apiSuccessClass: "apiSuccess",
+		apiParamClass: "apiParam"
+	};
 
-  constructor (type) {
-    this.ast = new Project()
-    this.type = type
-  }
+	constructor(type) {
+		this.ast = new Project();
+		this.type = type;
+	}
 
-  /**
-   * Entrance for apidoc plugin
-   * @param elements
-   * @param element
-   */
-  parseElements (elements, element) {
-    elements.pop()
-    this.extractElementContent(element.content)
-    const classAst = this.getClassAst({filePath: this.filePath, className: this.className})
-    if (!classAst) {
-      return elements
-    }
-    const newElements = this.transferToNewElement({filePath: this.filePath, classAst, prefix: ''})
+	/**
+	 * Entrance for apidoc plugin
+	 * @param elements
+	 * @param element
+	 */
+	parseElements(elements, element) {
+		elements.pop();
+		this.extractElementContent(element.content);
+		const classAst = this.getClassAst({ filePath: this.filePath, className: this.className });
+		if (!classAst) {
+			return;
+		}
+		const newElements = this.transferToNewElement({ filePath: this.filePath, classAst, prefix: "" });
 
-    elements.push(...newElements)
-  }
+		elements.push(...newElements);
+	}
 
-  /**
-   * Get apidoc elements from class ast
-   * @param param0
-   */
-  private transferToNewElement ({filePath, classAst, prefix}): IElement[] {
-    const properties = classAst.getProperties()
-    let elements: IElement[] = []
-    for (const property of properties) {
-      const name = prefix ? `${prefix}.${property.getName()}` : property.getName()
-      const node = property.getJsDocs()[0]
-      const comment = node ? node.getComment() : property.getName()
-      const description = prefix ? `${prefix} > ${comment}` : comment
-      let type = property.getType().getText()
-      let transferType = type
-      if (!this.isNativeType(type)) {
-        transferType = transferType.includes('[]') ? 'Object[]' : 'Object'
-      }
-      const content = `{${this.capitalize(transferType)}} ${name} ${description}`
-      const transferParserType = this.typeMap[this.type]
-      elements.push({
-        content,
-        name: transferParserType.toLowerCase(),
-        source: `@${transferParserType} ${content}\n`,
-        sourceName: transferParserType
-      })
-      if (!this.isNativeType(type)) {
-        const childAst = this.getClassAst({filePath, className: type.replace('[]', '')})
-        if (childAst) {
-          const childs = this.transferToNewElement({ filePath, classAst: childAst, prefix: name })
-          elements.push(...childs)
-        }
-      }
-    }
-    return elements
-  }
+	/**
+	 * Get apidoc elements from class ast
+	 * @param param0
+	 */
+	private transferToNewElement({ filePath, classAst, prefix }): IElement[] {
+		const properties = classAst.getProperties();
+		let elements: IElement[] = [];
+		for (const property of properties) {
+			const name = prefix ? `${prefix}.${property.getName()}` : property.getName();
+			const node = property.getJsDocs()[0];
+			const comment = node ? node.getComment() : property.getName();
+			const description = prefix ? `${prefix} > ${comment}` : comment;
+			let type = property.getType().getText();
+			let transferType = type;
+			if (!this.isNativeType(type)) {
+				transferType = transferType.includes("[]") ? "Object[]" : "Object";
+			}
+			let content_type = this.capitalize(transferType, property);
+			const content = property.hasQuestionToken()
+				? `${content_type} [${name}] ${description}`
+				: `${content_type} ${name} ${description}`;
+			const transferParserType = this.typeMap[this.type];
+			elements.push({
+				content,
+				name: transferParserType.toLowerCase(),
+				source: `@${transferParserType} ${content}\n`,
+				sourceName: transferParserType
+			});
+			if (!this.isNativeType(type)) {
+				const childAst = this.getClassAst({ filePath, className: type.replace("[]", "") });
+				if (childAst) {
+					const childs = this.transferToNewElement({ filePath, classAst: childAst, prefix: name });
+					elements.push(...childs);
+				}
+			}
+		}
+		return elements;
+	}
 
-  /**
-   * Extract file path and class name from element definition
-   * @param content element definition
-   */
-  private extractElementContent (content: string) {
-    let startPos = content.indexOf('(')
-    let endPos = content.indexOf(')', startPos)
-    this.filePath = content.substring(startPos + 1, endPos)
-    startPos = content.indexOf('{', endPos)
-    endPos = content.indexOf('}', startPos)
-    this.className = content.substring(startPos + 1, endPos)
-  }
+	/**
+	 * Extract file path and class name from element definition
+	 * @param content element definition
+	 */
+	private extractElementContent(content: string) {
+		let startPos = content.indexOf("(");
+		let endPos = content.indexOf(")", startPos);
+		this.filePath = content.substring(startPos + 1, endPos);
+		startPos = content.indexOf("{", endPos);
+		endPos = content.indexOf("}", startPos);
+		this.className = content.substring(startPos + 1, endPos);
+	}
 
-  private getClassAst ({filePath, className}) {
-    const file = this.ast.addExistingSourceFile(filePath)
-    return file.getClass(className)
-  }
+	private getClassAst({ filePath, className }) {
+		const file = this.ast.addExistingSourceFile(filePath);
+		return file.getClass(className);
+	}
 
-  private capitalize (text) {
-    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
-  }
+	private capitalize(text, property) {
+		let content_type = "{" + text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+		if (property.getStructure().decorators) {
+			const min = property.getStructure().decorators.find(decorator => decorator.name == "MinLength" || decorator.name == "Min");
+			const max = property.getStructure().decorators.find(decorator => decorator.name == "MaxLength" || decorator.name == "Max");
+			const allow = property.getStructure().decorators.find(decorator => decorator.name == "IsIn");
+			if ((min || max) && allow) {
+				content_type += ` {${min.arguments[0] || ""}~${max.arguments[0] || ""}}=${allow.arguments[0].replace("[","").replace("]","")} }`;
+			} else if (min || max) {
+				content_type += ` {${min.arguments[0] || ""}~${max.arguments[0] || ""}} }`;
+			} else if (allow) {
+				content_type += `=${allow.arguments[0].replace("[", "").replace("]", "")} }`;
+			} else {
+				content_type += "}";
+			}
+		} else {
+			content_type += "}";
+		}
+		return content_type;
+	}
 
-  private isNativeType (propType) {
-    return ['boolean', 'string', 'number', 'Date', 'any'].includes(propType)
-  }
+	private isNativeType(propType) {
+		return ["boolean", "string", "number", "Date", "any"].includes(propType);
+	}
 }
